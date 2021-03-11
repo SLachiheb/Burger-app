2404f8d9684c66a4a2a9ebbbc73d5f1eecf76abe
Sarah Lachiheb <sarah.lachiheb@gmail.com>
2021-03-11 12:34:50 +0100

    adding a sidedrawer toggle button

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,19 +3,27 @@ import React from 'react';
 import style from './SideDrawer.module.css';
 import Logo from '../../UI/Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from './../../../hoc/Aux';
 
 const sideDrawer = props => {
-  //...
+  let attachedClass = [style.SideDrawer, style.Close];
+  if (props.open) {
+    attachedClass = [style.SideDrawer, style.Open];
+  }
   return (
-    <div className={style.SideDrawer}>
-      <div className={style.Logo}>
-        {/* <Logo height="11%" /> */}
-        <Logo />
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClass.join(' ')}>
+        <div className={style.Logo}>
+          {/* <Logo height="11%" /> */}
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   );
 };
 
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,10 +3,11 @@ import React from 'react';
 import style from './Toolbar.module.css';
 import Logo from '../../UI/Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = props => (
   <header className={style.Toolbar}>
-    <div>Menu</div>
+    <DrawerToggle clicked={props.drawerToggleClicked} />
     <div className={style.Logo}>
       <Logo />
     </div>

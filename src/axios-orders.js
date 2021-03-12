c776bead021dab6347e8059cb701cf7db3bd53f0
Sarah Lachiheb <sarah.lachiheb@gmail.com>
2021-03-12 10:30:18 +0100

    creating the axios instance

---
+++ src/axios-orders.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseUrl:
+    'https://react-burger-builder-4e679-default-rtdb.europe-west1.firebasedatabase.app/',
+});
+
+export default instance;

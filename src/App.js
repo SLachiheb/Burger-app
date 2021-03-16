8041180a2d26615dfcc71eac3d4366f006c6332e
Sarah Lachiheb <sarah.lachiheb@gmail.com>
2021-03-16 11:13:28 +0100

    building the checkout container

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/burgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 function App() {
   return (
     <div>
       <Layout>
         <BurgerBuilder />
+        <Checkout />
       </Layout>
     </div>
   );
---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import style from './CheckoutSummary.module.css';
+import Burger from './../../Burger/Burger';
+import Button from './../../UI/Button/Button';
+
+const checkoutSummary = props => {
+  return (
+    <div className={style.CheckoutSummary}>
+      <h1>We hope it tastes well!</h1>
+      <div style={{ with: '100%', height: '300px', margin: 'auto' }}>
+        <Burger ingredients={props.ingredients} />
+      </div>
+      <Button btnType="Danger" clicked>
+        Cancel
+      </Button>
+      <Button btnType="Success" clicked>
+        Continue
+      </Button>
+    </div>
+  );
+};
+
+export default checkoutSummary;
